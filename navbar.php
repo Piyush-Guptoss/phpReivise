51a928d3c88f3e17e4ecc8d9498bba2bd1878167
Piyush-Guptoss <spiderpiyush5@gmail.com>
2024-09-06 21:48:51 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<header>
+        <div class="container containernav">
+            <h2 class="navreivise" title="Reivise (home)">Reivise</h2>
+            <nav>
+                <div class="menu-toggle">
+
+                    <span class="bar1"></span>
+                    <span class="bar2"></span>
+                    <span class="bar3"></span>
+                </div>
+                <div class="navicons">
+                    <ul class="nav-links">
+                        <li><a href="blog_post.php">Blog</a></li>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#mcqs">MCQs</a></li>
+                        <li><a href="#solutions">Solutions</a></li>
+                        <li><a href="#tests">Online Tests</a></li>
+                        <li><a href="breng10thmcq.php">Contact</a></li>
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+
+    </header>
